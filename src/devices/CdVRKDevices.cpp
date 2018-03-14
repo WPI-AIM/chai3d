00ee862bd5cf4244ccfc8050de54a74b8416ca31
Adnan Munawar <adnan.munawar@live.com>
2018-03-14 15:37:46 -0400

    Calling the wrench method to avoid sleeping twice

--- src/devices/CdVRKDevices.cpp
+++ src/devices/CdVRKDevices.cpp
@@ -506,8 +506,7 @@ bool cDvrkDevice::setForceAndTorqueAndGripperForce(const cVector3d& a_force,
 
     double gf = a_gripperForce;
 
-    mtm_device->set_force(fx, fy, fz);
-    mtm_device->set_moment(0, 0, 0);
+    mtm_device->set_wrench(fx, fy, fz, 0, 0, 0);
     // setForceToMyDevice(fx, fy, fz);
     // setTorqueToMyDevice(tx, ty, tz);
     // setForceToGripper(fg);
