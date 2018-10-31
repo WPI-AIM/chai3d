a0739ed6d1fb768b19188caff9eaade9fc1efe6f
Adnan Munawar <adnan.munawar@live.com>
2018-10-30 23:30:44 -0400

    Increased time delay for polling hydra which gives greater stability

--- src/devices/CRazerHydraDevices.cpp
+++ src/devices/CRazerHydraDevices.cpp
@@ -523,7 +523,7 @@ bool cRazerHydraDevice::updateData()
     // update data
     if (m_timeguard.timeoutOccurred())
     {
-        m_hydra_dev->poll(1, 3);
+        m_hydra_dev->poll(5, 3);
         m_timeguard.start(true);
     }
 
